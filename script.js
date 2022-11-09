ca3012a154f99b9be7bd9c03eb7ad411ba58d3bb
abhinavkumar0711 <84240826+abhinavkumar0711@users.noreply.github.com>
2022-11-09 14:17:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// slideshow pictures
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    else if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
